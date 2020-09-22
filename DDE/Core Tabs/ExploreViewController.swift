b01d310a3795a45476fadddf5f964a02a7423a47
iljoo chae <9662964@gmail.com>
2020-09-22 14:59:55 -0400

    first commit

---
+++ DDE/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  DDE
+//
+//  Created by iljoo Chae on 9/22/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
